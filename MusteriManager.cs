f6e5475e58c525fc400c9c2d18be182624ace695
sahra <sahra@DESKTOP-LEHMEVA>
2021-02-19 23:17:23 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle()
+        {
+            Console.WriteLine("Yeni Müşteri Eklendi");
+        }
+        public void Sırala(Musteri[] musteriler)
+        {
+            
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Ad:" + musteri.Adi +"Bakiyesi"+ musteri.Bakiye);
+                Console.WriteLine("-------------------------");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Ayşe";
+            musteri1.Yasi = 25;
+            musteri1.Bakiye = 10000;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Adi = "Fatma";
+            musteri2.Yasi = 30;
+            musteri2.Bakiye = 50000;
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2};
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle();
+            musteriManager.Sırala(musteriler);
+            
+
+
+        }
+        
+    }
+}
